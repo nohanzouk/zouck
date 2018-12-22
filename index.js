4584cc134068b04533a17d55bc78c1ca1529d6b3
nohanzouk <46064908+nohanzouk@users.noreply.github.com>
2018-12-22 09:31:35 +0100

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const Discord = require('discord.js');
 const bot = new Discord.Client();
 const cfg require('./index.json'); // a garder en version desktop
 const token = process.env.token // a garder en version heroku
-const prefix = ("?");
+const prefix = ("z!");
 
 bot.on('ready', function () {
     console.log("Je suis prêt à être utilisé.")
@@ -11,7 +11,7 @@ bot.on('ready', function () {
 
 bot.on('guildMemberAdd', member => {
     member.createDM().then(channel => {
-        return channel.send('Bienvenue sur le serveur de Alexpgm' + member.displayName)
+        return channel.send('Bienvenue sur le serveur de Zouk' + member.displayName)
         console.log(`${member.displayName} à rejoind le serveur.`)
     }).catch(console.error)
 });
